c802d2b2f586091cf4f327d449c12fb670ecba77
Lars Juul <ljb@easv.dk>
2021-01-27 17:13:50 +0100

    Adding chat service and socket.io

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class ChatGateway {}
+export class ChatGateway {
+  @WebSocketServer() server;
+  @SubscribeMessage('message')
+  handleChatEvent(@MessageBody() data: string): string {
+    console.log(data);
+    this.server.emit('messages', data);
+    return data + ' Hello';
+  }
+}
