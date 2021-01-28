edeb5f82948cea7829686203aeb68e3f033cde6b
Lars Juul <ljb@easv.dk>
2021-01-28 18:00:09 +0100

    Adding Gateway Connect and Disconnect

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,17 +1,25 @@
 import {
-  MessageBody,
+  MessageBody, OnGatewayConnection, OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
   @SubscribeMessage('message')
   handleChatEvent(@MessageBody() data: string): string {
     console.log(data);
     this.server.emit('messages', data);
     return data + ' Hello';
+  }
+
+  handleConnection(client: any, ...args: any[]): any {
+    console.log('Client Connect', client.id);
+  }
+
+  handleDisconnect(client: any): any {
+    console.log('Client Disconnect', client.id);
   }
 }
