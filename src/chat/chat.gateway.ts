5af4964527459ce0c2682ff9b753a8145190b76d
scupak <anto1095@easv365.dk>
2021-02-03 11:46:30 +0100

    Update chat.gateway.ts

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,13 +1,16 @@
 import {
+  ConnectedSocket,
   MessageBody, OnGatewayConnection, OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import {Socket} from "socket.io";
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
+  private userMap: Map<string, string> = new Map<string, string>();
   @SubscribeMessage('message')
   handleChatEvent(@MessageBody() data: string): string {
     console.log(data);
@@ -15,11 +18,23 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     return data + ' Hello';
   }
 
+  @SubscribeMessage('name')
+  handleNameEvent(@MessageBody() name: string,
+    @ConnectedSocket() client: Socket,
+  ): string {
+    this.userMap.set(client.id, name);
+    this.server.emit('clients', Array.from(this.userMap.values()));
+    console.log('map ', this.userMap);
+    return name + ' Hello';
+  }
+
   handleConnection(client: any, ...args: any[]): any {
     console.log('Client Connect', client.id);
   }
 
   handleDisconnect(client: any): any {
+    this.userMap.delete(client.id);
+    this.server.emit('client', this.userMap.values())
     console.log('Client Disconnect', client.id);
   }
 }
