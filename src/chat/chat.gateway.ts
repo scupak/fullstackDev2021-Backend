9de9707a5b7ef64a7f15f797e127f14e14aeaca7
Lars Juul <ljb@easv.dk>
2021-02-06 15:08:26 +0100

    Adding clients support

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody, OnGatewayConnection, OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
@@ -9,6 +10,7 @@ import { Socket } from 'socket.io';
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   allMessages: string[] = [];
+  clients: Map<string, string> = new Map<string, string>();
   @WebSocketServer() server;
   @SubscribeMessage('message')
   handleChatEvent(@MessageBody() message: string): string {
@@ -18,12 +20,25 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     return message + ' Hello';
   }
 
+  @SubscribeMessage('nickname')
+  handleNicknameEvent(
+    @MessageBody() nickname: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    this.clients.set(client.id, nickname);
+    console.log('All Nicknames:', this.clients);
+    this.server.emit('clients', Array.from(this.clients.values()));
+  }
+
   handleConnection(client: Socket, ...args: any[]): any {
     console.log('Client Connect', client.id);
     client.emit('allMessages', this.allMessages);
+    this.server.emit('clients', Array.from(this.clients.values()));
   }
 
-  handleDisconnect(client: any): any {
-    console.log('Client Disconnect', client.id);
+  handleDisconnect(client: Socket): any {
+    this.clients.delete(client.id);
+    this.server.emit('clients', Array.from(this.clients.values()));
+    console.log('Client Disconnect', this.clients);
   }
 }
